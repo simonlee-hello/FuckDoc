373bc50d253e6c22fd998ecf1f4f0b6fdecc527b
simonlee-hello <594743010@qq.com>
2023-11-26 14:00:40 +0800

    优化；解决压缩包路径问题

--- Process/Process.cs
+++ Process/Process.cs
@@ -23,40 +23,40 @@ namespace FuckDoc.Process
                 Console.WriteLine($"Skipping directory: {rootPath}");
                 return;
             }
-
+        
             // 创建一个 ZIP 文件
             using (var zip = ZipFile.Open(zipFilePath, ZipArchiveMode.Create))
             {
                 // 递归处理目录中的文件和子目录
                 ProcessDirectoryRecursively(rootPath, zip, fileFilter);
             }
-
+        
             Console.WriteLine($"All files compressed successfully: {zipFilePath}; Total size is {FileUtil.GetFileSize(zipFilePath)}");
             
         }
-
+        
         private static void ProcessDirectoryRecursively(string directoryPath, ZipArchive zip, FileFilter fileFilter)
         {
             var directory = new DirectoryInfo(directoryPath);
-
+        
             
             // 处理目录中的文件
             foreach (var file in directory.GetFiles())
             {
- 
+        
                 
                 if (!fileFilter.Filter(file.FullName, file)) continue;
                 // Console.WriteLine(file.FullName);
                 try
                 {
-                    AddFileToZip(zip, file.FullName, fileFilter.FlagInfo.OutputPath);
+                    AddFileToZip(zip, file.FullName);
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine($"Skipped file {file.FullName} due to exception: {e.Message}");
                 }
             }
-
+        
             // 递归处理子目录
             foreach (var subDirectory in directory.GetDirectories())
             {
@@ -71,27 +71,20 @@ namespace FuckDoc.Process
                 
             }
         }
-
-        private static void AddFileToZip(ZipArchive zip, string sourceFilePath, string outputPath)
+        
+        private static void AddFileToZip(ZipArchive zip, string sourceFilePath)
         {
             
-                // Calculate the relative path inside the ZIP file
-                var relativePath = FileUtil.GetRelativePath(outputPath, sourceFilePath);
-                // Normalize the path separators in case the sourceFilePath uses backslashes
-                relativePath = relativePath.Replace("\\", "/");
-
-                // 在 ZIP 文件中创建一个条目并从源文件复制内容
-                // zip.CreateEntryFromFile(sourceFilePath, relativePath);
                 // 创建 ZIP 文件中的条目
-                var entry = zip.CreateEntry(relativePath);
-
+                var entry = zip.CreateEntry(sourceFilePath);
+        
                 // 打开文件流并将文件内容写入 ZIP 条目
                 using (var entryStream = entry.Open())
                 using (var sourceStream = File.OpenRead(sourceFilePath))
                 {
                     sourceStream.CopyTo(entryStream);
                 }
-
+        
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,42 +1,19 @@
-﻿using FuckDoc.Option;
+﻿// using FuckDoc.Option;
+// using FuckDoc.Utils;
+
+using FuckDoc.Option;
 using FuckDoc.Utils;
 
 namespace FuckDoc
 {
     public static class Program
     {
-        // TODO: 内存问题
         private static void Main()
         {
             var info = new FlagInfo();
             info.InitFlag();
             LogUtil.PrintFlagInfo(info);
             Process.Process.WalkAndProcess(info);
-            // Test2();
-            
         }
-
-        // static void Test()
-        // {
-        //     var r = Utils.FileUtil.IsFileExists("/Users/simon/Desktop/tmp/test/test.exe");
-        //     Console.WriteLine(r);
-        //     var size =Utils.FileUtil.GetTotalSize(new[] { "/Users/simon/Desktop/tmp/test/test.exe" });
-        //     Console.WriteLine(Utils.TransformUtil.BytesToSize(size));
-        //     // Utils.FileUtil.DeleteFile("/Users/simon/Desktop/tmp/test/test.exe");
-        //     string result = Utils.TransformUtil.TransformSlash("\\users\\alsdf\\asdf");
-        //     Console.WriteLine(result);
-        //     var convertStringToList = Utils.TransformUtil.ConvertStringToList("a,b,c,d,1234");
-        //     foreach (var s in convertStringToList)
-        //     {
-        //         Console.WriteLine(s);    
-        //     }
-        //
-        //     var stringToHashSet = Utils.TransformUtil.StringToHashSet("pdf,doc,xlsx");
-        //     foreach (var s in stringToHashSet)
-        //     {
-        //         Console.WriteLine(s);    
-        //     }
-        // }
-        
     }
 }
